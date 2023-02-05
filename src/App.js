97a16132b559dcdd3b08c8995eb7b8b2124e7df8
mitoyouhei <mitoyouhei@proton.me>
2023-02-05 23:58:00 +0800

    enable first contract call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FUND_ME } from "./contracts";
+import "./App.css";
+
+const { ethereum } = window;
+const { ethers } = window;
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}`);
+  return new Promise((resolve, reject) => {
+    try {
+      provider.once(transactionResponse.hash, (transactionReceipt) => {
+        console.log(
+          `Completed with ${transactionReceipt.confirmations} confirmations. `
+        );
+        resolve();
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+async function fund(amount) {
+  console.log(`Funding with ${amount}...`);
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(FUND_ME.address, FUND_ME.abi, signer);
+  try {
+    const transactionResponse = await contract.fund({
+      value: ethers.utils.parseEther(amount),
+    });
+    await listenForTransactionMine(transactionResponse, provider);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function FundMe({ account }) {
+  const [amount, setAmount] = useState(0.01);
+
+  function changeAmount(event) {
+    setAmount(event.target.value);
+  }
+  function submitFund(event) {
+    event.preventDefault();
+    const fundEth = parseFloat(amount);
+    console.log("fundEth:", fundEth);
+    fund(fundEth.toString());
+  }
+
+  return (
+    <div>
+      <p>
+        <strong>Account: {account}</strong>
+      </p>
+      <form className="row g-3" onSubmit={submitFund}>
+        <div className="col-auto">
+          <label htmlFor="inputAmount" className="visually-hidden">
+            Amount
+          </label>
+          <input
+            type="number"
+            step="any"
+            value={amount}
+            onChange={changeAmount}
+            className="form-control"
+            id="inputAmount"
+            placeholder="Amount"
+          />
+        </div>
+        <div className="col-auto">
+          <button type="submit" className="btn btn-primary mb-3">
+            Fund
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+async function ethRequest(args) {
+  const response = await ethereum.request(args);
+  console.log(`${args.method}:`, response);
+  return response;
+}
 
 function App() {
+  const [isConnectedWallet, setIsConnectedWallet] = useState(false);
+  const [account, setAccount] = useState(null);
+  async function updateConnectionStatus() {
+    const accounts = await ethRequest({ method: "eth_accounts" });
+    const isConnected = accounts.length > 0;
+    setIsConnectedWallet(isConnected);
+    if (isConnected) {
+      setAccount(accounts[0]);
+    }
+  }
+  updateConnectionStatus();
+  ethereum.on("accountsChanged", updateConnectionStatus);
+
+  async function connectWallet() {
+    await ethRequest({ method: "eth_requestAccounts" });
+    updateConnectionStatus();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App container">
+      <h1 className="display-1">Fund Me</h1>
+      {isConnectedWallet ? (
+        <FundMe account={account} />
+      ) : (
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={connectWallet}
         >
-          Learn React
-        </a>
-      </header>
+          Connect Wallet
+        </button>
+      )}
     </div>
   );
 }

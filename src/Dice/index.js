9dcd2e3677aa0d52cbbc4c0ee2b9d0c6da426706
Satoru Gojo <125535347+satorugojorich@users.noreply.github.com>
2023-02-20 21:40:22 +0800

    Show Loading

--- src/Dice/index.js
+++ src/Dice/index.js
@@ -21,22 +21,35 @@
 //      Call play / Shaking Dice
 //      Get Result / Stop Dice and show Result
 //
+
 improt withBackHome from "../enhancers/withBackHome";
 import Button from "../components/Button";
 import Loading from "../components/Loading";
+import './index.css';
 
 const Dice = React.memo(()=> {
+    const [loading, setLoading] = useState(false);
     const createRoom = useCallback(() => {
         console.log('createRoom');
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
     }, []);
     const refreshRoom = useCallback(() => {
         console.log('refreshRoom');
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
     }, []);
     const joinRoom = useCallback((roomId) => {
         console.log('joinRoom');
     }, []);
+
    return (
-    <div>
+    <div className="dice-container">
+        { loading && (<Loading />) }
         <Button onClick={createRoom} text={'Create Room'}/>
         <Button onClick={refreshRoom} text={'Refresh Room'}/>
         <Button onClick={joinRoom} text={'Join Room'}/>

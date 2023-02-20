6a7f4746399a103e04a1b366aba6d5469a33613e
Satoru Gojo <125535347+satorugojorich@users.noreply.github.com>
2023-02-20 21:21:12 +0800

    Add basic game journey of Dice

--- src/Dice/index.js
+++ src/Dice/index.js
@@ -1,3 +1,26 @@
+// User join:
+//      Game [Create Button]
+//      View all Dice status, with [Join Button][Refresh Button]
+// User Click [Create Button]:
+//      Choose Dice Type: big/small. (should be number)
+//      Connect Wallet / Loading
+//      Create Game / Loading
+//      Into Game Room / Done
+// User Click [Join Button]:
+//      Connect Wallet / Loading
+//      Join Game / Loaing
+//        - Failed by room fulfilled 
+//      Into Game Room / Done
+//      
+//
+// User In Game Room:
+//      Keep Polling room status
+//      One User in the Room, Show Waiting      
+//      Two User in the Room, Show Play
+// One of the User Click Play:
+//      Call play / Shaking Dice
+//      Get Result / Stop Dice and show Result
+//
 improt withBackHome from "../enhancers/withBackHome";
 
 const Dice = React.memo(()=> {

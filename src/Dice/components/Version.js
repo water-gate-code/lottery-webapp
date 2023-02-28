7a7faca064ae73c369000e678e0dc8a5f1e52194
Satoru <unknown>
2023-02-28 12:12:46 +0800

    Add version rule and add it to comment.

--- src/Dice/components/Version.js
+++ src/Dice/components/Version.js
@@ -2,11 +2,20 @@ import React from "react";
 import DICE from "../service/getContract";
 import './index.css';
 
+/**
+ * UI_VERSION = Major.Minor.BuildNumber　
+ * Major: 新功能，如：新增一种游戏，新支持一个链等
+ * Minor: 合约更新
+ * BuildNumber: 仅UI更新，或存量功能增强
+ */
+const UI_VERSION = '0.1.0';
+
 const Version = React.memo(() => {
+
   return (
-    <div className={'version'}>
+    <div className={ 'version' }>
       <span>{ `Contract Address: ${ DICE.address }` }</span>
-      <span>UI Version: 0.1.0</span>
+      <span>{ `UI Version: ${ UI_VERSION }` }</span>
     </div>
   )
 })

f75d20e2c13d12e0cd2867227a9236a293ca4e13
mitoyouhei <mitoyouhei@proton.me>
2023-03-03 12:17:22 +0800

    enable router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,104 @@
-import { useState, useMemo, useCallback } from "react";
-import FundMe from "./FundMe";
 import Dice from "./Dice";
-import Button from "./Dice/components/Button";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useRouteError,
+  Outlet,
+  Link,
+} from "react-router-dom";
 
 import "./App.css";
 import { AccountProvider } from "./Dice/context/Account";
 
-function App() {
-  document.title = "The Greate Water Gate";
-  const [currentGame, setCurrentGame] = useState("DICE");
-  const showAllGameEntry = useMemo(() => currentGame === "", [currentGame]);
-  const backHomeView = useCallback(() => {
-    setCurrentGame("");
-  }, [setCurrentGame]);
+function Topbar() {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to={`/`}>
+          Barsino
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
 
   return (
-    <div className="App container">
-      { showAllGameEntry && (
-        <div>
-          <h1 className="title"> The Greate Water Gate </h1>
-          <div className="game-list">
-            <Button text={ "FundMe" } onClick={ () => setCurrentGame("FUND_ME") }/>
-            <Button text={ "Dice" } onClick={ () => setCurrentGame("DICE") }/>
-          </div>
-        </div>
-      ) }
-      { currentGame === "FUND_ME" && <FundMe goBack={ () => backHomeView() }/> }
-      <AccountProvider>
-        { currentGame === "DICE" && <Dice goBack={ () => backHomeView() }/> }
-      </AccountProvider>
+    <div>
+      <Topbar />
+
+      <div
+        className="App container text-center"
+        style={{ width: 500, marginTop: 150 }}
+      >
+        <h1 className="display-6">Oops!</h1>
+        <p className="lead">Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
     </div>
   );
+}
+
+function Game() {
+  return <h1 class="display-1">Game Panel</h1>;
+}
+
+function Layout() {
+  return (
+    <div>
+      <Topbar />
+      <div className="row">
+        <div className="col-2">
+          <h1 className="display-6">Games</h1>
+          <div className="list-group">
+            <Link
+              className="list-group-item list-group-item-action"
+              to={`games/1`}
+            >
+              Game A
+            </Link>
+            <Link
+              className="list-group-item list-group-item-action"
+              to={`games/2`}
+            >
+              Game B
+            </Link>
+          </div>
+        </div>
+        <div className="col-10">
+          <div className="App container">
+            <Outlet />
+            {/* <AccountProvider>
+              <Dice />
+            </AccountProvider> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "games/:gameId",
+        element: <Game />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Dice/index.js
+++ src/Dice/index.js
@@ -54,14 +54,14 @@ const DicePlayGround = React.memo((props) => {
   const largeDisplayName = gamberLarge?.address
     ? gamberLarge.name
     : selection === "big"
-      ? "Me"
-      : "等待玩家加入";
+    ? "Me"
+    : "等待玩家加入";
 
   const smallDisplayName = gamberSmall?.address
     ? gamberSmall.name
     : selection === "small"
-      ? "Me"
-      : "等待玩家加入";
+    ? "Me"
+    : "等待玩家加入";
 
   const { diceShaking, shootDice, diceNumber } = useShootDice(
     DICE_WAGER,
@@ -89,36 +89,36 @@ const DicePlayGround = React.memo((props) => {
   return (
     <div className="row full-height">
       <div className="col-4 center">
-        { isEnd && (diceNumber > 3 ? <h4>Winnder</h4> : <h4>Loser</h4>) }
+        {isEnd && (diceNumber > 3 ? <h4>Winnder</h4> : <h4>Loser</h4>)}
         <h2>大</h2>
-        <div>{ largeDisplayName }</div>
+        <div>{largeDisplayName}</div>
       </div>
       <div className="col-4 center">
         <div
-          className={ `dice ${
+          className={`dice ${
             diceShaking ? "dice-start" : "dice-end"
-          } dice-${ diceNumber }` }
+          } dice-${diceNumber}`}
         />
-        { diceShaking && (
+        {diceShaking && (
           <button disabled type="button" className="btn btn-danger">
             正在摇骰子
           </button>
-        ) }
-        { isStart && (
-          <button type="button" className="btn btn-danger" onClick={ oShootDice }>
+        )}
+        {isStart && (
+          <button type="button" className="btn btn-danger" onClick={oShootDice}>
             掷骰子
           </button>
-        ) }
-        { isEnd && (
+        )}
+        {isEnd && (
           <button disabled type="button" className="btn btn-danger">
             结束
           </button>
-        ) }
+        )}
       </div>
       <div className="col-4 center">
-        { isEnd && (diceNumber < 4 ? <h4>Winnder</h4> : <h4>Loser</h4>) }
+        {isEnd && (diceNumber < 4 ? <h4>Winnder</h4> : <h4>Loser</h4>)}
         <h2>小</h2>
-        <div>{ smallDisplayName }</div>
+        <div>{smallDisplayName}</div>
       </div>
     </div>
   );
@@ -146,20 +146,20 @@ const DiceCreate = React.memo((props) => {
         <button
           type="button"
           className="btn btn-danger"
-          onClick={ () => createGame("big") }
+          onClick={() => createGame("big")}
         >
           选择
         </button>
       </div>
       <div className="col-4 center">
-        <div className={ "dice" }/>
+        <div className={"dice"} />
       </div>
       <div className="col-4 center">
         <h2>小</h2>
         <button
           type="button"
           className="btn btn-danger"
-          onClick={ () => createGame("small") }
+          onClick={() => createGame("small")}
         >
           选择
         </button>
@@ -178,28 +178,28 @@ const DiceList = React.memo((props) => {
     };
 
     return (
-      <div className="dice-item" key={ diceId }>
-        { gambers.map((gamber) => (
-          <div className="dice-gamber" key={ gamber.address }>
-            <h4>{ gamber.select === "big" ? "大" : "小" }</h4>
-            { gamber.address ? (
-              <div className="dice-gamber-name">{ gamber.name }</div>
+      <div className="dice-item" key={diceId}>
+        {gambers.map((gamber) => (
+          <div className="dice-gamber" key={gamber.address}>
+            <h4>{gamber.select === "big" ? "大" : "小"}</h4>
+            {gamber.address ? (
+              <div className="dice-gamber-name">{gamber.name}</div>
             ) : (
               <button
                 type="button"
                 className="btn btn-outline-danger"
-                onClick={ () => onSelectGame(gamber.select) }
+                onClick={() => onSelectGame(gamber.select)}
               >
                 选择
               </button>
-            ) }
+            )}
           </div>
-        )) }
+        ))}
       </div>
     );
   };
   return (
-    <div className={ "full-height" }>{ dice.map((die) => renderDetail(die)) }</div>
+    <div className={"full-height"}>{dice.map((die) => renderDetail(die))}</div>
   );
 });
 
@@ -207,13 +207,13 @@ const GameList = React.memo((props) => {
   const { joinGame, games } = props;
   return (
     <div className="game-list-container">
-      <DiceList dice={ games } join={ joinGame }/>
+      <DiceList dice={games} join={joinGame} />
     </div>
   );
 });
 
 const Dice = React.memo((props) => {
-  document.title = "Dice";
+  document.title = "Barsino - Dice";
   const [showNewGame, setShowNewGame] = useState(true);
   const [joinedGame, setJoinedGame] = useState(null);
   // 避免多次调用合约，selection 需要先存储下来，后续在开始掷骰子的时候一并调用合约的 play 方法
@@ -250,28 +250,28 @@ const Dice = React.memo((props) => {
   return (
     <div className="dice-container container">
       <Version />
-      { showLoading && <Loading/> }
+      {showLoading && <Loading />}
       <h1 className="title">掷骰子</h1>
-      {/* <Button onClick={goBack} text={'返回'} /> */ }
+      {/* <Button onClick={goBack} text={'返回'} /> */}
       <div className="row dice-content">
         <div className="col-3 navigation">
-          <Button onClick={ clickNewGame } text={ "新游戏" }/>
-          <Button onClick={ refetchGames } text={ "刷新游戏列表" }/>
-          <GameList joinGame={ onJoinedGame } games={ games }/>
+          <Button onClick={clickNewGame} text={"新游戏"} />
+          <Button onClick={refetchGames} text={"刷新游戏列表"} />
+          <GameList joinGame={onJoinedGame} games={games} />
         </div>
         <div className="col-9 playground full-height">
-          { showNewGame ? (
+          {showNewGame ? (
             <DiceCreate
-              creatGameAndPay={ creatGameAndPay }
-              setSelection={ setSelection }
-              refetchGames={ refetchGames }
+              creatGameAndPay={creatGameAndPay}
+              setSelection={setSelection}
+              refetchGames={refetchGames}
             />
           ) : (
             <DicePlayGround
-              dice={ createdGame || joinedGame }
-              selection={ selection }
+              dice={createdGame || joinedGame}
+              selection={selection}
             />
-          ) }
+          )}
         </div>
       </div>
     </div>

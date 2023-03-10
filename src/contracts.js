06d1bc5e3d28df53612220003036bd28d7063b33
mitoyouhei <mitoyouhei@proton.me>
2023-03-10 18:37:28 +0800

    udpate contract

--- src/contracts.js
+++ src/contracts.js
@@ -1,147 +1,6 @@
-export const DICE = {
-  RpcUrl: "HTTP://127.0.0.1:7545",
-  address: "0x52A14236635FEEbedE2506c8458b4Ec3d360e251",
-  abi: [
-    {
-      anonymous: false,
-      inputs: [
-        {
-          components: [
-            {
-              internalType: "uint256",
-              name: "id",
-              type: "uint256",
-            },
-            {
-              internalType: "address",
-              name: "player1",
-              type: "address",
-            },
-            {
-              internalType: "address",
-              name: "player2",
-              type: "address",
-            },
-            {
-              internalType: "uint256",
-              name: "betAmount",
-              type: "uint256",
-            },
-            {
-              internalType: "uint256",
-              name: "player1BetNumber",
-              type: "uint256",
-            },
-            {
-              internalType: "uint256",
-              name: "player2BetNumber",
-              type: "uint256",
-            },
-          ],
-          indexed: false,
-          internalType: "struct DiceGameLobby.DiceGame",
-          name: "game",
-          type: "tuple",
-        },
-      ],
-      name: "CreateGame",
-      type: "event",
-    },
-    {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "roll",
-          type: "uint256",
-        },
-      ],
-      name: "PlayGame",
-      type: "event",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "betNumber",
-          type: "uint256",
-        },
-      ],
-      name: "createGame",
-      outputs: [],
-      stateMutability: "payable",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "getGames",
-      outputs: [
-        {
-          components: [
-            {
-              internalType: "uint256",
-              name: "id",
-              type: "uint256",
-            },
-            {
-              internalType: "address",
-              name: "player1",
-              type: "address",
-            },
-            {
-              internalType: "address",
-              name: "player2",
-              type: "address",
-            },
-            {
-              internalType: "uint256",
-              name: "betAmount",
-              type: "uint256",
-            },
-            {
-              internalType: "uint256",
-              name: "player1BetNumber",
-              type: "uint256",
-            },
-            {
-              internalType: "uint256",
-              name: "player2BetNumber",
-              type: "uint256",
-            },
-          ],
-          internalType: "struct DiceGameLobby.DiceGame[]",
-          name: "",
-          type: "tuple[]",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "id",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256",
-          name: "betNumber",
-          type: "uint256",
-        },
-      ],
-      name: "play",
-      outputs: [],
-      stateMutability: "payable",
-      type: "function",
-    },
-  ],
-};
-
 export const CASINO = {
   RpcUrl: "HTTP://127.0.0.1:7545",
-  address: "0xd905E804eE5Ef495Bf5AEFbac98D717Cee2863CA",
+  address: "0x28e6Da9FC313356C8b22FBB61653FA688a3845c2",
   abi: [
     {
       inputs: [],
@@ -152,7 +11,7 @@ export const CASINO = {
       inputs: [
         {
           internalType: "uint256",
-          name: "gameId",
+          name: "gameType",
           type: "uint256",
         },
         {
@@ -183,9 +42,9 @@ export const CASINO = {
               type: "uint256",
             },
             {
-              internalType: "string",
+              internalType: "uint256",
               name: "gameType",
-              type: "string",
+              type: "uint256",
             },
             {
               components: [

7796b446a27b97c4993f9eea4cb2b83423b39269
mitoyouhei <mitoyouhei@proton.me>
2023-04-03 17:53:35 +0800

    enable polygon

--- src/chains.js
+++ src/chains.js
@@ -1,6 +1,6 @@
 import { CASINO } from "./contracts";
 
-export const supportChainIds = [80001, 1337];
+export const supportChainIds = [137, 80001, 1337];
 
 export const chains = {
   1337: {
@@ -80,6 +80,43 @@ export const chains = {
     contracts: {
       Casino: {
         address: "0x8eac226eCadA790A7B7e03E8187c805bdeAE1e71",
+        abi: CASINO.abi,
+      },
+    },
+  },
+  137: {
+    info: {
+      name: "Polygon Mainnet",
+      chain: "Polygon",
+      icon: "https://icons.llamao.fi/icons/chains/rsz_polygon.jpg",
+      rpc: [
+        "https://polygon-rpc.com/",
+        "https://rpc-mainnet.matic.network",
+        "https://matic-mainnet.chainstacklabs.com",
+        "https://rpc-mainnet.maticvigil.com",
+        "https://rpc-mainnet.matic.quiknode.pro",
+        "https://matic-mainnet-full-rpc.bwarelabs.com",
+        "https://polygon-bor.publicnode.com",
+      ],
+      nativeCurrency: { name: "MATIC", symbol: "MATIC", decimals: 18 },
+      infoURL: "https://polygon.technology/",
+      shortName: "matic",
+      chainId: 137,
+      networkId: 137,
+      explorers: [
+        {
+          name: "polygonscan",
+          url: "https://polygonscan.com",
+          standard: "EIP3091",
+        },
+      ],
+    },
+    local: false,
+    testNet: false,
+    nativeMinScale: 1,
+    contracts: {
+      Casino: {
+        address: "0xbB851Ddf38EE7Dd30737EbD5A30EAd463E508d3C",
         abi: CASINO.abi,
       },
     },

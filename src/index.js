300bb391376725f957e0108b9acee0d737682028
mitoyouhei <mitoyouhei@proton.me>
2023-04-04 14:53:29 +0800

    enable ga with vitals

--- src/index.js
+++ src/index.js
@@ -26,6 +26,23 @@ if (!window.ethereum) {
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals((vitals) => {
-  console.log("[Vitals]:", vitals);
-});
+reportWebVitals(sendToGoogleAnalytics);
+
+function sendToGoogleAnalytics({ name, delta, value, id }) {
+  // Assumes the global `gtag()` function exists, see:
+  // https://developers.google.com/analytics/devguides/collection/ga4
+  window.gtag("event", name, {
+    // Built-in params:
+    value: delta, // Use `delta` so the value can be summed.
+    // Custom params:
+    metric_id: id, // Needed to aggregate events.
+    metric_value: value, // Optional.
+    metric_delta: delta, // Optional.
+
+    // OPTIONAL: any additional params or debug info here.
+    // See: https://web.dev/debug-performance-in-the-field/
+    // metric_rating: 'good' | 'needs-improvement' | 'poor',
+    // debug_info: '...',
+    // ...
+  });
+}

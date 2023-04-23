8abc85c8011566b14c81717c61ef61e1c2a07d79
Mito Youhei <mitoyouhei@proton.me>
2023-04-23 15:44:07 +0800

    support uk lang

--- src/langs/index.ts
+++ src/langs/index.ts
@@ -2,10 +2,15 @@ import en from "./en.json";
 import ar from "./ar.json";
 import ms from "./ms.json";
 import th from "./th.json";
+import uk from "./uk.json";
 
 export const langs: {
   [lang: string]: { translation: object; displayName: string };
 } = {
+  uk: {
+    translation: uk,
+    displayName: "Українська мова",
+  },
   en: {
     translation: en,
     displayName: "English",
